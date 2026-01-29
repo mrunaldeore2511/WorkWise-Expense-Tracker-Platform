7c4d237bb93723d1b600b3bf50e1bae004772b0c
mrunaldeore2511 <mrunalddeore@gmail.com>
2026-01-29 21:53:37 +0530

    Fix React Router 404 on Vercel

--- server/index.js
+++ server/index.js
@@ -44,7 +44,7 @@ app.listen(PORT, () => {
 
 app.use(cors({
   origin: [
-    process.env.FRONTEND_URL,
+    'http://localhost:5173', // dev,
     'https://work-wise-expense-tracker-platform-99opb2cke.vercel.app'
   ],
   credentials: true
